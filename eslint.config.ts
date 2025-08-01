b09d940aa3ee2800b90476dd8ac302f7baea2324
Cozoob <kozubmarcin10@gmail.com>
2025-08-01 17:44:12 +0200

    Add eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,13 +1,8 @@
-import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import { globalIgnores } from 'eslint/config'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
   {
@@ -17,12 +12,122 @@ export default defineConfigWithVueTs(
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
-  
+
+  {
+    rules: {
+      'vue/block-lang': [
+        'error',
+        {
+          script: {
+            lang: 'ts',
+          },
+        },
+      ],
+      'vue/component-api-style': ['error', ['script-setup']],
+      'vue/component-name-in-template-casing': [
+        'error',
+        'PascalCase',
+        {
+          registeredComponentsOnly: true,
+          ignores: [],
+        },
+      ],
+      'vue/component-options-name-casing': ['error', 'PascalCase'],
+      'vue/custom-event-name-casing': [
+        'error',
+        'camelCase',
+        {
+          ignores: [],
+        },
+      ],
+      'vue/define-emits-declaration': ['error', 'type-based'],
+      'vue/define-macros-order': [
+        'error',
+        {
+          order: ['defineProps', 'defineEmits'],
+          defineExposeLast: false,
+        },
+      ],
+      'vue/define-props-declaration': ['error', 'type-based'],
+      'vue/define-props-destructuring': [
+        'error',
+        {
+          destructure: 'always',
+        },
+      ],
+      'vue/enforce-style-attribute': ['error', { allow: ['scoped'] }],
+      'vue/html-button-has-type': [
+        'error',
+        {
+          button: true,
+          submit: true,
+          reset: true,
+        },
+      ],
+      'vue/match-component-file-name': [
+        'error',
+        {
+          extensions: ['vue'],
+          shouldMatchCase: true,
+        },
+      ],
+      'vue/match-component-import-name': 'error',
+      'vue/max-lines-per-block': [
+        'warn',
+        {
+          template: 200,
+          script: 300,
+          style: 200,
+          skipBlankLines: true,
+        },
+      ],
+      'vue/max-props': ['warn', { maxProps: 5 }],
+      'vue/max-template-depth': ['warn', { maxDepth: 5 }],
+      'vue/new-line-between-multi-line-property': [
+        'error',
+        {
+          minLineOfMultilineProperty: 2,
+        },
+      ],
+      'vue/next-tick-style': ['error', 'promise'],
+      'vue/no-empty-component-block': 'error',
+      'vue/no-import-compiler-macros': 'error',
+      'vue/no-ref-object-reactivity-loss': 'error',
+      'vue/no-root-v-if': 'error',
+      'vue/no-undef-properties': 'error',
+      'vue/no-use-v-else-with-v-for': 'error',
+      'vue/no-unused-emit-declarations': 'error',
+      'vue/no-boolean-default': ['error', 'default-false'],
+      'vue/no-undef-components': [
+        'error',
+        {
+          ignorePatterns: [],
+        },
+      ],
+      'vue/no-unused-properties': 'error',
+      'vue/no-unused-refs': 'error',
+      'vue/prefer-prop-type-boolean-first': 'error',
+      'vue/prefer-define-options': 'error',
+      'vue/require-typed-object-prop': 'error',
+      'vue/require-typed-ref': 'error',
+      'vue/slot-name-casing': ['error', 'camelCase'],
+      'vue/v-for-delimiter-style': ['error', 'in'],
+      'vue/v-on-handler-style': [
+        'error',
+        ['method', 'inline-function'],
+        {
+          ignoreIncludesComment: false,
+        },
+      ],
+    },
+  },
+
   {
     ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
+    files: ['src/**/__tests__/*', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
   },
+
   skipFormatting,
 )
